a094c8243c521812147125c28cf408af0d7c83c1
WILTON WILL DE PAULO <57237004+wiltonwilldepaulo@users.noreply.github.com>
2021-09-08 21:09:25 -0400

    Finalizado a criação de diretorios da aplicação
    Finalizado a criação de diretorios da aplicação

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ use Slim\Factory\AppFactory;
 $app = AppFactory::create();
 
 $app->get('/', function ($request, $response) {
-    $response->getBody()->write("Hello");
+    $response->getBody()->write("cliente");
     return $response;
 });
 
